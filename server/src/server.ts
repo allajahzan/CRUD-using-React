2b7f9c428d3782a5d27ec35e3bdfbda506793538
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2024-10-02 13:21:12 +0530

    changed cors setup again

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URL as string)
 // app
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://localhost:3000/', 
     credentials: true,              
 }));
 app.use(express.static('public'))
